f06fc0941d8427fbb4657d0842309fb580da5558
Alena <alenzija@gmail.com>
2024-01-27 13:18:19 +0100

    feat: implement cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const argv = process.argv;
+    for (let i = 0; i < argv.length; i += 1) {
+        if(/^--/.test(argv[i])) {
+            console.log(`${argv[i].replace(/^--/,'')} is ${argv[i + 1]}`);
+            i += 1;
+        }
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    Object.entries(env).forEach(([key, value]) => {
+        if (/^RSS_/.test(key)) {
+            console.log(`${key}=${value}`);
+        }
+    })
 };
 
 parseEnv();
