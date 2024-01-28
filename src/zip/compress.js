541dd30bb641bd691dc2d9fd7a133a676d040134
Alena <alenzija@gmail.com>
2024-01-28 17:51:42 +0100

    feat: implement zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import {
+  createReadStream,
+  createWriteStream,
+} from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+const __dirname = path.resolve();
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'src', 'zip', 'files', 'fileToCompress.txt');
+    const fileOutputPath = path.join(__dirname, 'src', 'zip', 'files', 'archive.gz');
+
+    const gzip = createGzip();
+    const source = createReadStream(filePath);
+    const destination = createWriteStream(fileOutputPath);
+
+    pipe(source, gzip, destination).catch((err) => {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+      });
+    ;
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import { createUnzip } from 'zlib';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import {
+    createReadStream,
+    createWriteStream,
+  } from 'fs';
+import path from 'path';
+
+const __dirname = path.resolve();
+const pipe = promisify(pipeline);
+
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = path.join(__dirname, 'src', 'zip', 'files', 'archive.gz');
+    const input = createReadStream(inputFilePath);
+    
+    const outputFilePath = path.join(__dirname, 'src', 'zip', 'files', 'fileToCompressV2.txt');
+    const output = createWriteStream(outputFilePath);
+
+    const unzip = createUnzip();
+
+    pipe(input, unzip, output).catch((err) => {
+        console.error('An error occurred:', err);
+        process.exitCode = 1;
+      });
+    ;
 };
 
 await decompress();
