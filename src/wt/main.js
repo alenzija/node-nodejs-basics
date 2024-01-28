c71c9b599f8a7db32e52a559054af40e603b5976
Alena <alenzija@gmail.com>
2024-01-28 21:28:23 +0100

    feat: implement worker threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
-const performCalculations = async () => {
-    // Write your code here
+import { Worker, isMainThread } from 'worker_threads';
+import path from 'path';
+import os from 'os';
+
+const __dirname = path.resolve();
+
+const performCalculations = () => {
+    const filePath = path.join(__dirname, 'src', 'wt', 'worker.js');
+    
+    if (isMainThread) {
+        const numCores = os.cpus().length;
+    
+        for (let i = 0; i < numCores; i++) {
+          const worker = new Worker(filePath, { workerData: i + 1 });
+    
+          worker.on('message', (message) => {
+            console.log(`Worker ${worker.threadId} finished with message status: ${message.status} & message data ${message.data}`);
+          });
+    
+          worker.on('error', (error) => {
+            console.log(`Worker ${worker.threadId} encountered an error: ${error}`);
+          });
+    
+          worker.on('exit', (code) => {
+            if (code !== 0) {
+              console.log(`Worker ${worker.threadId} exited with code ${code}`);
+            }
+          });
+    
+          worker.postMessage(10 + i);
+        }
+      }
 };
 
-await performCalculations();
+performCalculations();
