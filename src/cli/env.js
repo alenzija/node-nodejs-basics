ac67649d8ecda46e36fcb3878bf8a4dc8dfc9985
Alena <alenzija@gmail.com>
2025-04-27 22:07:51 +0200

    feat: create cli functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssVariables = Object.entries(process.env).filter(([key,_]) => /^RSS_/.test(key));
+    const rssVariablesString = rssVariables.reduce((acc, [key, value]) => `${acc} ${key}=${value}; `, '').replace(/; $/, '');
+    console.log(rssVariablesString);
 };
 
 parseEnv();
