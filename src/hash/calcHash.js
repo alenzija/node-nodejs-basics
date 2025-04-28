09683065798f6daaecc5a6ff035ee5a8d9b9a48a
Alena <alenzija@gmail.com>
2025-04-28 09:14:15 +0200

    feat: create calculateHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('error', console.error);
+
+    stream.on('close', () => {
+        console.log(hash);
+    })
+
 };
 
 await calculateHash();
