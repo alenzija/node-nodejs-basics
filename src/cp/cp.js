adb17700af4733c721e4f642b1d906b319f03aac
Alena <alenzija@gmail.com>
2025-04-28 10:38:25 +0200

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from 'node:child_process';
+import { dirname, join } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [filePath, ...args]);
+
+    stdin.pipe(child.stdin);
+
+    child.stdout.pipe(stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
