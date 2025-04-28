07c2e24c2878e506985427ecf679e84d0ccf8c46
Alena <alenzija@gmail.com>
2025-04-28 09:44:15 +0200

    feat: add streams functions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { stdin } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(filePath);
+
+    stdin.pipe(stream);
+    
+    stream.on('error', console.error);
 };
 
 await write();
