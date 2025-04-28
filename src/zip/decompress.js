cb5785ecc085e6db30260e4c0dc287e9797999b5
Alena <alenzija@gmail.com>
2025-04-28 09:55:31 +0200

    feat: add zip functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createGunzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToCompress.txt');
+const compressedFilePath = join(__dirname, 'files', 'archive.gz');
+
 const decompress = async () => {
-    // Write your code here 
+        const readStream = createReadStream(compressedFilePath);
+        const writeStream = createWriteStream(filePath);
+    
+        const gunZip = createGunzip();
+    
+        readStream.pipe(gunZip).pipe(writeStream);
+    
+        readStream.on('error', console.error);
+        writeStream.on('error', console.error);
+        gunZip.on('error', console.error);
 };
 
 await decompress();
